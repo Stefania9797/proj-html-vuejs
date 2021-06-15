cc6fd79a656b2992d63814af96d726e2349608bb
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-15 12:18:37 +0200

    link inseriti con vue

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("ciao");
+var root= new Vue({
+    el:"#root",
+    data:{
+        topLinks:[
+            {item: "Sign in",},{item: "About us",},{item: "Contact us",},{item: "Buy now",}
+        ],
+        bottomLinks:[
+            {item: "Home",},{item: "Elements",},{item: "Features",},{item: "pages",},{item: "portfolio",},{item:"blog"},{item:"shop"}
+        ]
+    }
+})
